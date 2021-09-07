cc867457c01f618a523edb02a22fc018504837f5
Fredrik Busklein <fredrbus@stud.ntnu.no>
2021-09-07 19:29:25 +0200

    Oppgave 1 ferdig

---
+++ oppgave1.py
@@ -0,0 +1,45 @@
+number_list = [
+    1722,
+    979,
+    235,
+    366,
+    578,
+    299,
+    675,
+    1245,
+    744,
+    1834,
+    1456
+]
+
+# I denne oppgaven har du fått en liste med tall.
+#
+# To av disse tallene blir til sammen 2021, og oppgaven din er å finne ut hvilke to!
+#
+# Bruk kode-skjellettet under, og endre koden slik at man får svaret ved å kjøre
+#
+#       finn_to_tall(number_list)
+#
+# (Oppgaven er inspirert av dag 1 i Advent of Code 2020)
+#
+
+
+def finn_to_tall(numbers: list):
+    goal = 2021
+    num_1 = 0
+    num_2 = 0
+
+    #
+    #   Gjør magien din her!
+    #
+
+    return [num_1, num_2]
+    #
+    #   LF:
+    #
+    # x.sort(reverse=True)
+    # for i in range(len(x)):
+    #     for k in range(i + 1, len(x)):
+    #         if x[i] + x[k] == goal:
+    #             return [x[i], x[k]]
+    # return -1
